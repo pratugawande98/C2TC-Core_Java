38b66fe99c815d563de085afbf5e6855f040ea81
Admin <Admin@DESKTOP-G101JLL>
2022-07-07 14:16:35 +0530

    First commit

--- javacode/src/com/cg/Demo/Array.java
+++ javacode/src/com/cg/Demo/Array.java
@@ -2,9 +2,16 @@ package com.cg.Demo;
 
 public class Array {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		int[][] x=new int[2][3];
+		System.out.println(x);
+		System.out.println(x[0]);
+		System.out.println(x[0][0]);
+		System.out.println(x[0][1]);
+
+
 
 	}
-
+}
 }
